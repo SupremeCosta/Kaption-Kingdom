909dfc5c37d337f8734f3a1b2be25656dd2b97dd
spereira15 <105558705+spereira15@users.noreply.github.com>
2023-05-15 18:30:06 -0500

    added copy btn functionality and optimized js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var quoteEl = $('#quote-text')
 var authorEl = $('#quote-author')
 var twitterBtnEl = $('#twitter-button')
+var copyBtnEl = $('#copy-button')
 
 // I commented out the actual variables for now. When we have the API working,
 // I will uncomment them and delete the placeholder variables:
@@ -14,14 +15,34 @@ var quoteText = 'I like pizza'
 var authorText = 'Me'
 
 
-// Function for Tweet Button
-function shareTweet() {
-    twitterBtnEl.attr('href', 'https://twitter.com/intent/tweet?text=%22' + quoteText + '%22%20--%20' + authorText)
-}
-
-// Event listener for Tweet Button
-twitterBtnEl.on('click', shareTweet)
+// Twitter Button functionality
+twitterBtnEl.on('click', function () {
+  twitterBtnEl.attr('href', 'https://twitter.com/intent/tweet?text=%22' + quoteText + '%22%20--%20' + authorText)
+})
 
 
-// Function for Facebook Button
-// WIP
+// Facebook Button funcitonality
+
+
+// Copy Button funcitonality
+copyBtnEl.on('click', function () {
+  var text = quoteText + " --" + authorText;
+
+  // Create 'input' element with the generated text as its value
+  var tempInput = $('<input>');
+  tempInput.val(text);
+
+  // Append the 'input' element to the 'body' element
+  $('body').append(tempInput);
+
+  // Select the 'input' element with the generated text, copy it
+  // with the execCommand method of the document object, and delete
+  // the 'input' element after it is copied to the clipboard
+  tempInput.select();
+  document.execCommand('copy');
+  tempInput.remove();
+
+  // This alert is a placeholder. We have to change it to a 
+  // modal to let the user know the Text was copied to the clipboard.
+  alert('Text copied to clipboard!');
+});
